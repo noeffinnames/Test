db8d94835653b28e1b6bc533262170e991bff6b5
noeffinnames <ben.j.smyth@gmail.com>
2014-05-19 14:44:38 +1000

    test of first commit to github, committing some nonsense.

---
+++ ConsoleApp/src/com/example/test/consoleapp/MainProgram.java
@@ -0,0 +1,21 @@
+package com.example.test.consoleapp;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		printWords();
+	}
+	
+	private static void printWords() {
+		printWord("snuh");
+		printWord("bah");
+		printWord("hee");
+	}
+	
+	private static void printWord(String word) {
+		System.out.println(word);
+	}
+
+}
